34376a6eaf8500311546e679e581bcbe270d08e4
Erkan <etaylan@pm.me>
2020-05-13 09:01:51 +0000

    Create SelectableDataGridBehaviour.cs

---
+++ SelectableDataGridBehaviour.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Windows.Controls;
+using Microsoft.Xaml.Behaviors;
+
+public class SelectableDataGridBehaviour : Behavior<DataGrid>
+{
+    public event EventHandler DataGridSelectionChanged;
+
+    protected override void OnAttached()
+    {
+        AssociatedObject.SelectionChanged += AssociatedObjectOnSelectionChanged;
+    }
+
+    private void AssociatedObjectOnSelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        foreach (object item in e.AddedItems)
+        {
+            if (item is ISelectable selectable)
+            {
+                selectable.IsSelected = true;
+            }
+        }
+
+        foreach (object item in e.RemovedItems)
+        {
+            if (item is ISelectable selectable)
+            {
+                selectable.IsSelected = false;
+            }
+        }
+
+        DataGridSelectionChanged?.Invoke(sender, e);
+    }
+
+
+    protected override void OnDetaching()
+    {
+        AssociatedObject.SelectionChanged -= AssociatedObjectOnSelectionChanged;
+    }
+}
+
+    public interface ISelectable
+    {
+        bool IsSelected { get; set; }
+    }
